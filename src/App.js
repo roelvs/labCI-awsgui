db61c9d6efff5dccacd122c7d8bc5f5fd9282690
Lieven De Troch <ldetroch@outlook.com>
2020-04-23 22:45:23 +0200

    more sections

--- src/App.js
+++ src/App.js
@@ -16,15 +16,18 @@ function App() {
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
         <div>
           <NavBar />
+          <main>
+            <ServerList region="us-east-1" token={token} apiHost={apiHost}/>
+            <ServerList region="us-east-2" token={token} apiHost={apiHost}/>
+            <ServerList region="us-west-1" token={token} apiHost={apiHost}/>
+            <ServerList region="us-west-2" token={token} apiHost={apiHost}/>
 
-          <ServerList region="us-west-1" token={token} apiHost={apiHost}/>
-          <ServerList region="us-west-2" token={token} apiHost={apiHost}/>
-          <ServerList region="ap-east-1" token={token} apiHost={apiHost}/>
-          <ServerList region="ap-south-1" token={token} apiHost={apiHost}/>
-          <ServerList region="ap-northeast-3" token={token} apiHost={apiHost}/>
-          <ServerList region="ap-northeast-2" token={token} apiHost={apiHost}/>
-          <ServerList region="ap-southeast-1" token={token} apiHost={apiHost}/>
-          <ServerList region="ap-southeast-2" token={token} apiHost={apiHost}/>
+            <ServerList region="ap-east-1" token={token} apiHost={apiHost}/>
+            <ServerList region="ap-south-1" token={token} apiHost={apiHost}/>
+            <ServerList region="ap-northeast-3" token={token} apiHost={apiHost}/>
+            <ServerList region="ap-northeast-2" token={token} apiHost={apiHost}/>
+            <ServerList region="ap-southeast-1" token={token} apiHost={apiHost}/>
+            <ServerList region="ap-southeast-2" token={token} apiHost={apiHost}/>
 
             <ServerList region="eu-central-1" token={token} apiHost={apiHost}/>
             <ServerList region="eu-west-1" token={token} apiHost={apiHost}/>
